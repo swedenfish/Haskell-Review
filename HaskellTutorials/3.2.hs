375405f375511bdce23394ed6b4251ad3c7eb3c4
CharlesWang <hcw320@ic.ac.uk>
2022-07-15 11:09:32 +0100

    3.2 Q2 extensions, Q3

--- HaskellTutorials/3.2.hs
+++ HaskellTutorials/3.2.hs
@@ -7,3 +7,23 @@ remove _ [] = []
 remove e ((f,s):xs)
     | e == f = remove e xs
     | otherwise = (f,s) : (remove e xs)
+
+--using list comprehension
+remove' :: Eq a => a -> [(a,b)] -> [(a,b)]
+remove' e list = [(x,y) | (x,y) <- list, x /= e]
+
+--using filter
+remove'' :: Eq a => a -> [(a,b)] -> [(a,b)]
+remove'' e list = filter (\(a,b) -> fst(a,b)/=e) list
+--((/= e) . fst)
+--remove'' e list = filter p list
+--    where
+--        p (a,b) = (a/=e)
+
+--Q3
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = (quickSort l1) ++ [x] ++ (quickSort l2)
+    where
+        l1 = [y | y <- xs, y <= x]
+        l2 = [z | z <- xs, z > x]
