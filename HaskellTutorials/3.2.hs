cb917d27ef4c4858e23671781d771377e23bf9c0
CharlesWang <hcw320@ic.ac.uk>
2022-07-15 11:43:44 +0100

    3.2 Q4 Q5-half Q6-half

--- HaskellTutorials/3.2.hs
+++ HaskellTutorials/3.2.hs
@@ -1,3 +1,4 @@
+
 --Q1
 findAll x t = [y | (x',y) <- t, x' == x]
 
@@ -27,3 +28,22 @@ quickSort (x:xs) = (quickSort l1) ++ [x] ++ (quickSort l2)
     where
         l1 = [y | y <- xs, y <= x]
         l2 = [z | z <- xs, z > x]
+
+--Q4
+allSplits :: [a] -> [([a],[a])]
+allSplits list = [(splitAt n list) | n <- [1..length list-1]]
+
+--Q5
+prefixes :: [t] -> [[t]]
+prefixes list = [take n list | n <- [1..length list]]
+
+-- TODO: map version?
+--prefixes' :: [t] ->[[t]]
+--prefixes' list = map take (length list)
+
+--Q6
+substrings :: String -> [String]
+substrings [] = []
+substrings (x:xs) = prefixes (x:xs) ++ substrings xs
+
+--TODO: funcitional version and list comprehension version
